538578f4c81c4fc6522378b982dc035eb3399d20
pravallikanakkagit <pravallikakkc@gmail.com>
2025-06-11 15:21:56 -0500

    changes are made as per pravallika suggestions

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,9 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
-	int i=10;
+	private By cart= By.id("cart");
 	
 	public void cart() {
 		System.out.println("cart method");
